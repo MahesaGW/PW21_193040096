1cc609e0d3ae4f527ec36e3d731fab3b9a9cdcda
MahesaGW <mahesaagw@gmail.com>
2021-09-20 01:08:00 +0700

    Menyelesaikan pelajaran tentang index php

---
+++ Pertemuan 2/index.php
@@ -0,0 +1,13 @@
+[00.37, 20/9/2021] Momski 💍: <?php
+/*
+Mahesa Ghoni Wijaya
+193040096
+https://github.com/MahesaGW/pw21_193040096.git
+pertemuan 2 - 20 September 2021
+Mempelajari mengenai sintaks PHP 
+*/   
+?>
+<?php
+// Standar Output
+//echo, print
+?>
